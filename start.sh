94c80279e3ca16577b10f7d374477971d1dfe683
XstrangeX <120216933+DoctorStrangeOP@users.noreply.github.com>
2023-03-05 17:59:11 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Wahyu213/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/DoctorStrangeOP/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Wahyu213/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/DoctorStrangeOP/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
